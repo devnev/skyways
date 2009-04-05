993686f3b1f4b7851f367e17f75e6f1ce2cdb2e9
Mark Nevill <mark.nevill@gmail.com>
2009-04-05 19:22:10 +0200

    Added drawing of quad using ship coordinates.

--- src/window.cpp
+++ src/window.cpp
@@ -70,5 +70,14 @@ void Window::resizeGL( int width, int height )
 
 void Window::paintGL()
 {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLoadIdentity();
+	glTranslated( ship.xpos(), ship.zpos(), -40 );
+	glBegin( GL_QUADS );
+		glVertex3d( 0, 0, 0 );
+		glVertex3d( 1, 0, 0 );
+		glVertex3d( 1, -1, 0 );
+		glVertex3d( 0, -1, 0 );
+	glEnd();
 	std::cout << ship.xpos() << ", " << ship.zpos() << ", " << ship.speed() << std::endl;
 }
