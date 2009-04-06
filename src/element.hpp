701f3f36e784f76d4e660694b89195871a84d2ad
Mark Nevill <mark.nevill@gmail.com>
2009-04-06 18:38:39 +0200

    Cleaned up Element to use Point.

--- src/element.hpp
+++ src/element.hpp
@@ -8,27 +8,29 @@ class Element
 public:
 
 	Element( double x, double y, double z, double l, Block * b )
-		: _xoff( x ), _yoff( y ), _zoff( z ), _length( l ), _block( b )
+		: _pos(Point::make( x,  y,  z)), _length( l ), _block( b )
 	{
 	}
 
 	void glDraw()
 	{
 		glPushMatrix();
-		glTranslated( _xoff, _yoff, -_zoff );
+		glTranslated( _pos.x, _pos.y, -_pos.z );
 		glScaled( 1, 1, _length );
 		_block->glDraw();
 		glPopMatrix();
 	}
 
-	double xoff() const throw() { return _xoff; }
-	double yoff() const throw() { return _yoff; }
-	double zoff() const throw() { return _zoff; }
+	const Point& pos() const throw() { return _pos; }
+	double xoff() const throw() { return _pos.x; }
+	double yoff() const throw() { return _pos.y; }
+	double zoff() const throw() { return _pos.z; }
 	double length() const throw() { return _length; }
 
 private:
 
-	double _xoff, _yoff, _zoff, _length;
+	Point _pos;
+	double _length;
 	Block * _block;
 
 };
