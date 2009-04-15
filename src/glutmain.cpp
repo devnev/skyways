b07bbdde43f6ca3a66ec6d0b6ea78d00cc7190a2
Mark Nevill <mark.nevill@gmail.com>
2009-04-15 10:34:20 +0100

    Added GPLv2 licensing.

--- src/glutmain.cpp
+++ src/glutmain.cpp
@@ -1,3 +1,22 @@
+//
+// This file is part of the game Skyways.
+// Copyright (C) 2009 Mark Nevill
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -103,7 +122,7 @@ int main( int argc, char * argv[] )
 	glutInit( &argc, argv );
 	glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
 	glutInitWindowSize( 800, 600 );
-	glutCreateWindow( "skyways" );
+	glutCreateWindow( "Skyways" );
 
 	glutReshapeFunc( resize );
 	glutDisplayFunc( display );
