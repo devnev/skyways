fc238197a897c7fb2c5389e2ad8d5d45ed79b087
Mark Nevill <mark.nevill@gmail.com>
2009-05-27 14:43:09 +0200

    Fixed level generation so blocks don't overlap.

--- src/map.cpp
+++ src/map.cpp
@@ -88,13 +88,25 @@ void Map::generateMap()
 	elements.push_back(Element(
 		0, -1, 0, 20, block( "" ), Vector3( 1, 1, 1 )
 	));
+
+	size_t width = 7;
+	size_t base = 0;
+	std::vector< size_t > running(width, 0);
+	running[width/2] = 20;
+
 	for (size_t i = 0; i < 100; ++i)
 	{
+		size_t distance = rand()%18+8;
+		size_t length = rand()%20+2;
+		size_t col;
+		do {
+			col = rand()%width;
+		} while (running[col] > 0);
 		elements.push_back(Element(
-			rand() % 7 - 3,
+			(int)col - ((int)width)/2,
 			((double)(rand() % 5))/4 - 0.5,
-			rand() % 400,
-			rand() % 20 + 2,
+			base + distance,
+			length,
 			block( "" ),
 			Vector3(
 				( (double)rand() ) / RAND_MAX,
@@ -102,6 +114,13 @@ void Map::generateMap()
 				( (double)rand() ) / RAND_MAX
 			)
 		));
+		running[col] += distance + length;
+		size_t mindist = distance + length;
+		for (size_t j = 0; j < width; ++j)
+			mindist = std::min(mindist, running[j]);
+		for (size_t j = 0; j < width; ++j)
+			running[j] -= mindist;
+		base += mindist;
 	}
 }
 
