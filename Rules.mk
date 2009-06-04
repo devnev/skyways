2cf926930c8293c32c902f16e40486b98da95a52
Mark Nevill <mark.nevill@gmail.com>
2009-06-04 21:16:07 +0200

    Made Rules.mk more modular by allowing various hooks.

--- Rules.mk
+++ Rules.mk
@@ -1,3 +1,4 @@
+#!/usr/bin/make
 
 default: all
 
@@ -6,10 +7,18 @@ SRCDIR:=.
 BUILDDIR:=.
 DEPDIR:=.deps
 
+#{{{ load configuration
+#{{{ include build configuration
+
 include $(BUILDDIR)/Config.mk
 
-define enter_directory
+#}}}
+#{{{ directory enter and leave hooks
+
+define checkdir_enter_tpl
   $$(if $$(directory),,$$(error Set the `directory` variable before including this makefile in other makefiles))
+endef
+define enter_enter_tpl
   supsp := $$(sp)
   sp := $$(lastsp).x
   lastsp := $$(sp)
@@ -18,26 +27,45 @@ define enter_directory
   d := $$(directory)
   DIRECTORIES := $$(DIRECTORIES) $$(d)
 endef
-
-define leave_directory
-  SUBDIRS_$$(sp) := $$(SUBDIRS)
+define subdirs_enter_tpl
   SUBDIRS :=
-  $$(if $$(SUBDIRS_$$(sp)),$$(eval $$(call include_subdir_list,$$(SUBDIRS_$$(sp)))),)
-  d := $$(dirstack_$$(sp))
-  sp := $$(supsp)
+endef
+ENTER_TEMPLATES := checkdir enter subdirs $(ENTER_TEMPLATES)
+define enter_directory
+  $(foreach tpl,$(ENTER_TEMPLATES),$$(eval $$(call $(tpl)_enter_tpl)))
 endef
 
 define include_subdir
   directory := $$(d)/$(1)
   include $$(SRCDIR)/$$(directory)/Dir.mk
 endef
-
 define include_subdir_list
   $$(foreach subdir,$(1),$$(eval $$(call include_subdir,$$(subdir))))
 endef
+define subdirs_leave_tpl
+  SUBDIRS_$$(sp) := $$(SUBDIRS)
+  $$(if $$(SUBDIRS_$$(sp)),$$(eval $$(call include_subdir_list,$$(SUBDIRS_$$(sp)))),)
+  SUBDIRS :=
+endef
+define leave_leave_tpl
+  d := $$(dirstack_$$(sp))
+  sp := $$(supsp)
+endef
+LEAVE_TEMPLATES := $(LEAVE_TEMPLATES) subdirs leave
+define leave_directory
+  $(foreach tpl,$(LEAVE_TEMPLATES),$$(eval $$(call $(tpl)_leave_tpl)))
+endef
+
+#}}}
+#{{{ include top directory configuration
 
 directory := .
 include $(SRCDIR)/Dir.mk
+
+#}}}
+#}}}
+#{{{ process configuration
+#{{{ per-directory-and-module hooks
 
 define csrcall_moddir_tpl
   CSOURCES_$(1)_$(2):=$$(CSOURCES_$(1)_$(2)) $$(CSOURCES_$(2))
@@ -58,10 +86,12 @@ define objects_moddir_tpl
 endef
 MODDIR_TEMPLATES := $(MODDIR_TEMPLATES) \
 	csrcall_moddir_tpl cxxsrcall_moddir_tpl hdrall_moddir_tpl objects_moddir_tpl
-
 define process_module_directory
   $$(foreach tpl,$$(MODDIR_TEMPLATES),$$(eval $$(call $$(tpl),$(1),$(2))))
 endef
+
+#}}}
+#{{{ per-modules hooks
 
 define FLAGS_mod_template
   $$(foreach flag,CFLAGS CXXFLAGS CPPFLAGS LDFLAGS LIBS,$$(eval $$(flag)_$(1):=$$($$(flag)) $$($(1)_$$(flag)) $$($$(flag)_$(1))))
@@ -81,11 +111,13 @@ define LD_mod_template
 	$$(LINK.o) $$(LDFLAGS_$(1)) $$(LIBS_$(1)) $$^ $$(LOADLIBES) $$(LDLIBS) -o $$@
 endef
 MOD_TEMPLATES := $(MOD_TEMPLATES) FLAGS_mod_template C_mod_template CXX_mod_template LD_mod_template
-
 define process_module
   $$(foreach sp,$$(sp_list),$$(eval $$(call process_module_directory,$(1),$$(sp))))
   $$(foreach tpl,$$(MOD_TEMPLATES),$$(eval $$(call $$(tpl),$(1))))
 endef
+
+#}}}
+#{{{ process module list
 
 define process_modules
   $$(foreach mod,$(1),$$(eval $$(call process_module,$$(mod))))
@@ -94,8 +126,12 @@ define process_modules
   #DEPENDS:=$$(foreach obj,$$(OBJECTS),$$(dir $$(obj))$$(DEPDIR)/$$(basename $$(obj)).d)
   DEPENDS:=$$(join $$(dir $$(OBJECTS)),$$(addprefix $$(DEPDIR)/,$$(addsuffix .d,$$(basename $$(notdir $$(OBJECTS))))))
 endef
-
 $(eval $(call process_modules,$(MODULES)))
+
+#}}}
+#}}}
+#{{{ misc
+
 -include $(DEPENDS)
 
 CLEAN:=$(CLEAN) $(OBJECTS) $(BINARIES) $(DEPENDS)
@@ -114,3 +150,5 @@ force:
 $(SRCDIR)/Rules.mk: force
 	@mkdir -p $(patsubst %,$(BUILDDIR)/%,$(DIRECTORIES)) $(patsubst %,$(BUILDDIR)/%/$(DEPDIR),$(DIRECTORIES))
 
+#}}}
+# vim: fdm=marker
